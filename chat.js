deacadd10f76522ec1b9a43c26a385ca3c57e24c
Gloria-Wgy <gloriawang0314@gmail.com>
2025-09-03 18:13:12 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,16 +1,1 @@
-export const config = {
-  runtime: "nodejs",   // 确保用 Node.js runtime
-};
 
-export default function handler(req, res) {
-  const origin = req.headers.origin;
-  res.setHeader("Access-Control-Allow-Origin", origin || "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") {
-    return res.status(204).end();
-  }
-
-  res.status(200).json({ msg: "Hello World" });
-}
